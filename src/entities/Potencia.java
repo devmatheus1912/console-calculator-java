1d2e1b494a801572c45555ee7a1dbda444b87747
devmatheus1912 <matheusos1912@icloud.com>
2026-03-19 15:16:58 -0300

    refactor: permite expoente negativo na operação de potência

--- src/entities/Potencia.java
+++ src/entities/Potencia.java
@@ -4,17 +4,16 @@ public class Potencia implements OperacaoBinaria {
 
     @Override
     public double calcular(double a, double b) {
-        if (b < 0) {
-            throw new ArithmeticException("Expoente não pode ser negativo");
-        }
         return Math.pow(a, b);
     }
+
     @Override
     public String getNome() {
         return "Potência";
     }
+
     @Override
-    public String formatar (double a, double b, double resultado) {
+    public String formatar(double a, double b, double resultado) {
         return a + " ^ " + b + " = " + resultado;
     }
 }
